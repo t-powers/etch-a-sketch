cf5d39720b993e7c2e4fbacffe6f16953f2c03a4
t-powers <127955989+t-powers@users.noreply.github.com>
2024-04-09 18:21:45 -0400

    Update js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,17 @@
-for (let i = 0; i < 4; i++) {
-  let jsDiv = document.createElement("div");
-  jsDiv.innerHTML = "Hello, World";
-  document.body.appendChild(jsDiv);
+const N = 16;
+
+function addDiv() {
+  for (let i = 0; i < N; i++) {
+    const newDiv = document.createElement("div");
+
+    // Set the new div's id
+    newDiv.id = "myNewDiv";
+
+    newDiv.innerHTML = " ";
+
+    // Get the parent div element
+    const parentDiv = document.getElementById("myParentDiv");
+
+    parentDiv.appendChild(newDiv);
+  }
 }
