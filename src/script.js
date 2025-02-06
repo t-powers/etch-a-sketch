858b9197fed722ecf55932b6b0865ceecafd057d
t-powers <127955989+t-powers@users.noreply.github.com>
2025-02-05 21:04:05 -0500

    Fixed parent width to 90%

--- src/script.js
+++ src/script.js
@@ -3,6 +3,6 @@ const parentDiv = document.querySelector("#parentDiv");
 for (let i = 0; i < 256; i++) {
   const squareDiv = document.createElement("div");
   squareDiv.classList.add("flexGrid");
-  squareDiv.textContent = "div";
+
   parentDiv.appendChild(squareDiv);
 }
