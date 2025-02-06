109ed76a8d0e9666785bd7b606067338c1b7a20e
t-powers <127955989+t-powers@users.noreply.github.com>
2025-02-05 21:05:40 -0500

    Line cleanup

--- src/script.js
+++ src/script.js
@@ -3,6 +3,5 @@ const parentDiv = document.querySelector("#parentDiv");
 for (let i = 0; i < 256; i++) {
   const squareDiv = document.createElement("div");
   squareDiv.classList.add("flexGrid");
-
   parentDiv.appendChild(squareDiv);
 }
