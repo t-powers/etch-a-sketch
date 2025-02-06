0267f6969abeb6d571c22e6fd80f36ad4c3d13a9
t-powers <127955989+t-powers@users.noreply.github.com>
2025-02-05 21:00:47 -0500

    Created 16x16 flex grid

--- src/script.js
+++ src/script.js
@@ -2,5 +2,7 @@ const parentDiv = document.querySelector("#parentDiv");
 
 for (let i = 0; i < 256; i++) {
   const squareDiv = document.createElement("div");
-  console.log(squareDiv);
+  squareDiv.classList.add("flexGrid");
+  squareDiv.textContent = "div";
+  parentDiv.appendChild(squareDiv);
 }
