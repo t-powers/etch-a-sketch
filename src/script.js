5ecc25e2a3b51977a4bfb45fde07120a3f9d6171
t-powers <anthony_powers@icloud.com>
2025-03-02 16:29:02 -0500

    Update

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 const parentDiv = document.querySelector("#parentDiv");
-const gridDiv = document.querySelector(".flexGrid");
 const gridSizeBtn = document.querySelector("button");
 
 function colorGridElement() {
@@ -8,21 +7,22 @@ function colorGridElement() {
   squareDiv.addEventListener("mouseover", () => {
     squareDiv.style.backgroundColor = "black";
   });
+  let gridSize = parentSize;
+  for (let i = 0; i < gridSize * gridSize; i++) {}
   parentDiv.appendChild(squareDiv);
 }
 
 function openPrompt() {
-  let customSize = prompt("Enter integer for custom grid size. 1 to 100");
+  let customSize = parseInt(
+    prompt("Enter integer for custom grid size. 1 to 100")
+  );
   let parentSize = 400 / customSize;
   if (customSize > 0 && customSize <= 100) {
     alert(`Making new grid with size of ${customSize}`);
   } else {
     alert("Try again, use 1-100");
   }
-  let gridSize = parentSize;
-  for (let i = 0; i < gridSize**2; i++) {
-    colorGridElement();
-  }
+  colorGridElement(customSize);
 }
 
 gridSizeBtn.addEventListener("click", openPrompt);
