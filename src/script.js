8f334613d80273c27af61a531497ef8d9a1a4d76
t-powers <anthony_powers@icloud.com>
2025-03-04 21:10:16 -0500

    Added grid for sizing

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
 const gridButton = document.querySelector(".gridButton");
+const container = document.querySelector(".container");
 
 function gridSize() {
   const gridSize = prompt("Enter grid size, 1-100");
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
   console.log(gridSize);
 }
 
