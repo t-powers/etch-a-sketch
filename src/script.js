18ec04243612f1abe84034032727d4aecab0f966
t-powers <anthony_powers@icloud.com>
2025-03-04 19:53:28 -0500

    Added button classes, started grid function

--- src/script.js
+++ src/script.js
@@ -1,2 +1,8 @@
+const gridButton = document.querySelector(".gridButton");
 
+function gridSize() {
+  const gridSize = prompt("Enter grid size, 1-100");
+  console.log(gridSize);
+}
 
+gridButton.addEventListener("click", gridSize);
