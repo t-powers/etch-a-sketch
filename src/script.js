cbd06e8f8f8a65e4d8517ca5f0b2cc6c82514f7e
t-powers <anthony_powers@icloud.com>
2025-03-05 21:35:34 -0500

    Started grid element function

--- src/script.js
+++ src/script.js
@@ -5,7 +5,14 @@ function gridSize() {
   const gridSize = prompt("Enter grid size, 1-100");
   container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
-  console.log(gridSize);
+  createGrid();
+}
+
+function createGrid() {
+  const gridEl = document.createElement("div");
+  gridEl.style.backgroundColor = "black";
+  gridEl.classList.add("gridEl");
+  container.appendChild(gridEl);
 }
 
 gridButton.addEventListener("click", gridSize);
