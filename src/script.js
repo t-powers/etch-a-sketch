480ee425115356a73e3742ad5730e660ad712972
t-powers <127955989+t-powers@users.noreply.github.com>
2025-05-15 11:33:00 -0400

    Add js content

--- src/script.js
+++ src/script.js
@@ -1,1 +1,5 @@
-
+const container= document.querySelector('.container');
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = "Hello World";
+container.appendChild(content);
