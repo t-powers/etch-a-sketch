e0d2764821ebd5cb394f89454795a8edaedce10f
t-powers <127955989+t-powers@users.noreply.github.com>
2025-05-21 18:30:39 -0400

    Combines div variable to prompt

--- src/script.js
+++ src/script.js
@@ -3,17 +3,15 @@ const grid = document.querySelector(".grid");
 const gridSizeButton = document.querySelector(".gridSizeButton");
 
 gridSizeButton.addEventListener("click", () => {
-  const gridSize = prompt("Choose a number between 0 and 64.");
-  alert(gridSize);
-});
-
-for (let i = 0; i < 4; i++) {
-  for (let j = 0; j < 4; j++) {
-    const gridSquare = document.createElement("div");
-    gridSquare.classList.add("grid");
-    container.appendChild(gridSquare);
-    gridSquare.addEventListener("mouseover", (e) => {
-      e.target.style.background = "black";
-    });
+  const gridSize = parseInt(prompt("Choose a number between 0 and 64."));
+  for (let i = 0; i < gridSize; i++) {
+    for (let j = 0; j < gridSize; j++) {
+      const gridSquare = document.createElement("div");
+      gridSquare.classList.add("grid");
+      container.appendChild(gridSquare);
+      gridSquare.addEventListener("mouseover", (e) => {
+        e.target.style.background = "black";
+      });
+    }
   }
-}
+});
