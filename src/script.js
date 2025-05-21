a90283d2ff973a827447d0698fa6a90604a526c1
t-powers <127955989+t-powers@users.noreply.github.com>
2025-05-21 14:52:40 -0400

    Renames button class

--- src/script.js
+++ src/script.js
@@ -1,5 +1,11 @@
 const container = document.querySelector(".container");
 const grid = document.querySelector(".grid");
+const gridSizeButton = document.querySelector(".gridSizeButton");
+
+gridSizeButton.addEventListener("click", () => {
+  const gridSize = prompt("Choose a number between 0 and 64.");
+  alert(gridSize);
+});
 
 for (let i = 0; i < 4; i++) {
   for (let j = 0; j < 4; j++) {
