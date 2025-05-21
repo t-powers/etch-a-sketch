314341d2463c162830bab5b7756cb0d17f75105d
t-powers <127955989+t-powers@users.noreply.github.com>
2025-05-20 20:07:11 -0400

    Targeted event class and blackened the background

--- src/script.js
+++ src/script.js
@@ -1,9 +1,13 @@
 const container = document.querySelector(".container");
+const grid = document.querySelector(".grid");
 
 for (let i = 0; i < 4; i++) {
   for (let j = 0; j < 4; j++) {
     const gridSquare = document.createElement("div");
     gridSquare.classList.add("grid");
     container.appendChild(gridSquare);
+    gridSquare.addEventListener("mouseover", (e) => {
+      e.target.style.background = "black";
+    });
   }
 }
