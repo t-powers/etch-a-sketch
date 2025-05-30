8b8278e3413da7181a5e6c3f3e45dcb745b6d254
t-powers <127955989+t-powers@users.noreply.github.com>
2025-05-30 11:12:10 -0400

    update loop

--- src/script.js
+++ src/script.js
@@ -1,3 +1,14 @@
 const container = document.querySelector(".container");
-const gridSizeButton = document.querySelector(".gridSizeButton");
 
+const setValue = 5;
+
+for (let i = 0; i < setValue; i++) {
+  for (let h = 0; h < setValue; h++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.style.border = "1px solid black";
+    square.style.width = "238px";
+    square.style.height = "238px";
+    container.appendChild(square);
+  }
+}
