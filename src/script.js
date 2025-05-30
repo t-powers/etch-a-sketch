602304e98d3927ba39b5a26f38c1b6a258411bb1
t-powers <127955989+t-powers@users.noreply.github.com>
2025-05-30 11:35:53 -0400

    Removed border from child element

--- src/script.js
+++ src/script.js
@@ -1,14 +1,17 @@
 const container = document.querySelector(".container");
 
-const setValue = 5;
+const setValue = 64;
+const containerH = 956;
+const containerW = 956;
+const childDivHeight = parseInt(containerH / setValue)-2;
+const childDivWidth = parseInt(containerW / setValue)-2;
 
 for (let i = 0; i < setValue; i++) {
   for (let h = 0; h < setValue; h++) {
     let square = document.createElement("div");
     square.classList.add("square");
-    square.style.border = "1px solid black";
-    square.style.width = "238px";
-    square.style.height = "238px";
+    square.style.width = childDivWidth + "px";
+    square.style.height = childDivHeight + "px";
     container.appendChild(square);
   }
 }
